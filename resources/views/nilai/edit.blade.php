1134b808ad3dbca824ce635bf7aa71cbca665a03
Hendra Muswara <hendra.muswara@dev.batam.go.id>
2023-11-07 16:11:27 +0700

    readonly keterangan dan nilai akhir

--- resources/views/nilai/edit.blade.php
+++ resources/views/nilai/edit.blade.php
@@ -91,6 +91,30 @@
        name="uas" value="{{$nilai->uas}}" required>
     </div>
   </div>
+
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Nilai Akhir
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-red-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"
+       name="#" value="{{$nilai->total_nilai}}" readonly>
+    </div>
+  </div>
+
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Keterangan
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input class="bg-gray-200 appearance-none border-2 border-red-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"
+       name="#" value="{{$nilai->keterangan}}" readonly>
+    </div>
+  </div>
  
   
   <div class="md:flex md:items-center">
