d388198fee2c294cdf8c893159709e807a5c18c8
Hendra Muswara <hendra.muswara@dev.batam.go.id>
2023-11-09 15:41:53 +0700

    New Databases

--- pendidikan.sql
+++ pendidikan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 07:51 AM
+-- Generation Time: Nov 09, 2023 at 09:41 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -44,7 +44,7 @@ CREATE TABLE `failed_jobs` (
 --
 
 CREATE TABLE `guru` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_guru` int(11) NOT NULL,
   `namaguru` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `jeniskelamin` varchar(255) NOT NULL,
@@ -56,10 +56,8 @@ CREATE TABLE `guru` (
 -- Dumping data for table `guru`
 --
 
-INSERT INTO `guru` (`id`, `namaguru`, `alamat`, `jeniskelamin`, `updated_at`, `created_at`) VALUES
-(1, 'HenMus', 'Sarmen', 'Laki Laki', NULL, NULL),
-(2, 'Marisha', 'Marina', 'Perempuan', NULL, NULL),
-(5, 'Edi Prayitno', 'Tiban Lama', 'Laki Laki', '2023-11-08 05:52:02', '2023-11-08 05:52:02');
+INSERT INTO `guru` (`id_guru`, `namaguru`, `alamat`, `jeniskelamin`, `updated_at`, `created_at`) VALUES
+(6, 'Hendra Muswara', 'Sarmen', 'Laki Laki', '2023-11-09 08:06:49', '2023-11-09 08:04:27');
 
 -- --------------------------------------------------------
 
@@ -69,12 +67,19 @@ INSERT INTO `guru` (`id`, `namaguru`, `alamat`, `jeniskelamin`, `updated_at`, `c
 
 CREATE TABLE `jadwal` (
   `id_jadwal` int(11) NOT NULL,
-  `mapel` varchar(225) NOT NULL,
-  `kelas` varchar(225) NOT NULL,
+  `mapel` int(11) NOT NULL,
+  `kelas` int(11) NOT NULL,
   `jam` int(11) NOT NULL,
   `updated_at` datetime NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jadwal`
+--
+
+INSERT INTO `jadwal` (`id_jadwal`, `mapel`, `kelas`, `jam`, `updated_at`, `created_at`) VALUES
+(3, 3, 16, 10, '2023-11-09 08:33:55', '2023-11-09 08:33:55');
 
 -- --------------------------------------------------------
 
@@ -85,8 +90,7 @@ CREATE TABLE `jadwal` (
 CREATE TABLE `kelas` (
   `id_kelas` int(11) NOT NULL,
   `namakelas` varchar(255) NOT NULL,
-  `walikelas` varchar(255) NOT NULL,
-  `ketuakelas` varchar(255) NOT NULL,
+  `id_guru` int(11) NOT NULL,
   `kursi` int(11) NOT NULL,
   `meja` int(11) NOT NULL,
   `gambar_kelas` blob NOT NULL,
@@ -98,10 +102,8 @@ CREATE TABLE `kelas` (
 -- Dumping data for table `kelas`
 --
 
-INSERT INTO `kelas` (`id_kelas`, `namakelas`, `walikelas`, `ketuakelas`, `kursi`, `meja`, `gambar_kelas`, `updated_at`, `created_at`) VALUES
-(11, 'XII PPLG 1', 'HenMus', 'Ahmad Ramadhani', 30, 30, 0x313639383930353831362e706e67, '2023-11-08 05:52:20', '2023-11-02 06:16:56'),
-(12, 'XI PPLG 1', 'Edi Prayitno', 'Franklin Sebastian', 28, 28, 0x313639383937393039382e6a7067, '2023-11-03 02:38:18', '2023-11-02 07:04:11'),
-(14, 'X Seni Tari', 'Marisha', 'Putra Alamsyah', 15, 15, 0x313639393332333536302e6a7067, '2023-11-07 02:21:06', '2023-11-07 02:19:20');
+INSERT INTO `kelas` (`id_kelas`, `namakelas`, `id_guru`, `kursi`, `meja`, `gambar_kelas`, `updated_at`, `created_at`) VALUES
+(16, 'XI PPLG 2', 6, 15, 15, 0x313639393531373439312e706e67, '2023-11-09 08:11:31', '2023-11-09 08:11:31');
 
 -- --------------------------------------------------------
 
@@ -112,7 +114,7 @@ INSERT INTO `kelas` (`id_kelas`, `namakelas`, `walikelas`, `ketuakelas`, `kursi`
 CREATE TABLE `mapel` (
   `id_mapel` int(11) NOT NULL,
   `namamapel` varchar(225) NOT NULL,
-  `guru` varchar(225) NOT NULL,
+  `id_guru` int(11) NOT NULL,
   `updated_at` datetime NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -121,8 +123,8 @@ CREATE TABLE `mapel` (
 -- Dumping data for table `mapel`
 --
 
-INSERT INTO `mapel` (`id_mapel`, `namamapel`, `guru`, `updated_at`, `created_at`) VALUES
-(1, 'WEB', 'HenMus', '2023-11-08 02:20:52', '2023-11-08 02:19:54');
+INSERT INTO `mapel` (`id_mapel`, `namamapel`, `id_guru`, `updated_at`, `created_at`) VALUES
+(3, 'WEB', 6, '2023-11-09 08:28:49', '2023-11-09 08:28:49');
 
 -- --------------------------------------------------------
 
@@ -158,8 +160,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `nilai` (
   `id_nilai` int(11) NOT NULL,
-  `nama_siswa` varchar(225) NOT NULL,
-  `kelas` varchar(225) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
   `tugas` int(11) NOT NULL,
   `kuis` int(11) NOT NULL,
   `uts` int(11) NOT NULL,
@@ -174,9 +175,8 @@ CREATE TABLE `nilai` (
 -- Dumping data for table `nilai`
 --
 
-INSERT INTO `nilai` (`id_nilai`, `nama_siswa`, `kelas`, `tugas`, `kuis`, `uts`, `uas`, `total_nilai`, `keterangan`, `updated_at`, `created_at`) VALUES
-(4, 'Dini Aminarty', 'X Seni Tari', 80, 90, 75, 85, 83, 'B Baik', '2023-11-07 09:14:29', '2023-11-07 08:53:25'),
-(5, 'Ibra Aditiaa', 'XI PPLG 1', 100, 100, 100, 100, 100, 'A Sangat Bagus', '2023-11-07 08:59:43', '2023-11-07 08:57:01');
+INSERT INTO `nilai` (`id_nilai`, `id_siswa`, `tugas`, `kuis`, `uts`, `uas`, `total_nilai`, `keterangan`, `updated_at`, `created_at`) VALUES
+(7, 5, 100, 100, 100, 30, 72, 'D Cukup', '2023-11-09 08:40:52', '2023-11-09 08:40:30');
 
 -- --------------------------------------------------------
 
@@ -205,6 +205,31 @@ CREATE TABLE `password_reset_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pengembangan`
+--
+
+CREATE TABLE `pengembangan` (
+  `id_pengembangan` int(11) NOT NULL,
+  `namapengembangan` varchar(225) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `jenis` varchar(225) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengembangan`
+--
+
+INSERT INTO `pengembangan` (`id_pengembangan`, `namapengembangan`, `jumlah`, `jenis`, `updated_at`, `created_at`) VALUES
+(2, 'Osis', 40, 'Organisasi', '2023-11-09 07:05:22', '2023-11-09 07:04:55'),
+(3, 'Rohis', 56, 'Organisasi', '2023-11-09 07:05:41', '2023-11-09 07:05:41'),
+(4, 'Band', 10, 'Ekstrakulikuler', '2023-11-09 07:05:53', '2023-11-09 07:05:53'),
+(5, 'English Club', 78, 'Intrakulikuler', '2023-11-09 07:06:17', '2023-11-09 07:06:17');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `personal_access_tokens`
 --
 
@@ -228,11 +253,11 @@ CREATE TABLE `personal_access_tokens` (
 --
 
 CREATE TABLE `siswa` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_siswa` int(11) NOT NULL,
   `namasiswa` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `jeniskelamin` varchar(255) NOT NULL,
-  `kelas` varchar(255) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -241,10 +266,8 @@ CREATE TABLE `siswa` (
 -- Dumping data for table `siswa`
 --
 
-INSERT INTO `siswa` (`id`, `namasiswa`, `alamat`, `jeniskelamin`, `kelas`, `updated_at`, `created_at`) VALUES
-(1, 'Ahmad Ramadhani', 'Bengkong Harapan', 'Laki Laki', 'XII PPLG 1', NULL, NULL),
-(2, 'Ibra Aditiaa', 'Bengkong Nusantara', 'Laki Laki', 'XII PPLG 1', '2023-11-03 03:53:29', '2023-11-02 07:05:42'),
-(3, 'Dini Aminarty', 'Batu Merah', 'Perempuan', 'XII PPLG 1', '2023-11-03 03:28:30', '2023-11-03 03:28:06');
+INSERT INTO `siswa` (`id_siswa`, `namasiswa`, `alamat`, `jeniskelamin`, `id_kelas`, `updated_at`, `created_at`) VALUES
+(5, 'Dini Aminarty', 'sengkuang', 'Perempuan', 16, '2023-11-09 08:21:38', '2023-11-09 08:21:38');
 
 -- --------------------------------------------------------
 
@@ -282,7 +305,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(2, 'Hendra Muswara', 'henmus@gmail.com', NULL, '$2y$10$wfoXN4TlN9/H.AxXkoe08uBd9DsfzsAppCgb7kLAzwClAZNC7eZym', NULL, NULL, NULL);
+(2, 'Hendra Muswaraa', 'henmus@gmail.com', NULL, '$2y$10$wfoXN4TlN9/H.AxXkoe08uBd9DsfzsAppCgb7kLAzwClAZNC7eZym', NULL, NULL, '2023-11-08 01:31:56'),
+(4, '123', '123@mail.com', NULL, '$2y$12$1zdo5XKQwN0PfalNV/MOJOq/DSWbMJkmVuRXwJQ3Xy3pz1pXnyx9K', NULL, '2023-11-08 01:34:19', '2023-11-08 01:34:19');
 
 --
 -- Indexes for dumped tables
@@ -299,25 +323,30 @@ ALTER TABLE `failed_jobs`
 -- Indexes for table `guru`
 --
 ALTER TABLE `guru`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_guru`);
 
 --
 -- Indexes for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD PRIMARY KEY (`id_jadwal`);
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `mapel` (`mapel`,`kelas`),
+  ADD KEY `jadwalkelas` (`kelas`);
 
 --
 -- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_kelas`);
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `walikelas` (`id_guru`),
+  ADD KEY `id_guru` (`id_guru`);
 
 --
 -- Indexes for table `mapel`
 --
 ALTER TABLE `mapel`
-  ADD PRIMARY KEY (`id_mapel`);
+  ADD PRIMARY KEY (`id_mapel`),
+  ADD KEY `id_guru` (`id_guru`);
 
 --
 -- Indexes for table `migrations`
@@ -329,7 +358,8 @@ ALTER TABLE `migrations`
 -- Indexes for table `nilai`
 --
 ALTER TABLE `nilai`
-  ADD PRIMARY KEY (`id_nilai`);
+  ADD PRIMARY KEY (`id_nilai`),
+  ADD KEY `id_siswa` (`id_siswa`);
 
 --
 -- Indexes for table `password_resets`
@@ -344,6 +374,12 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `pengembangan`
+--
+ALTER TABLE `pengembangan`
+  ADD PRIMARY KEY (`id_pengembangan`);
+
+--
 -- Indexes for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
@@ -355,7 +391,9 @@ ALTER TABLE `personal_access_tokens`
 -- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_siswa`),
+  ADD KEY `kelas` (`id_kelas`),
+  ADD KEY `id_kelas` (`id_kelas`);
 
 --
 -- Indexes for table `user`
@@ -384,25 +422,25 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -414,7 +452,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `pengembangan`
+--
+ALTER TABLE `pengembangan`
+  MODIFY `id_pengembangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -426,7 +470,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -438,7 +482,42 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwalkelas` FOREIGN KEY (`kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jadwalmapel` FOREIGN KEY (`mapel`) REFERENCES `mapel` (`id_mapel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `GURU` FOREIGN KEY (`id_guru`) REFERENCES `guru` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mapel`
+--
+ALTER TABLE `mapel`
+  ADD CONSTRAINT `gurumapel` FOREIGN KEY (`id_guru`) REFERENCES `guru` (`id_guru`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilaisiswa` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `Kelas` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
